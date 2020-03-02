be3fe194f8e02572ff40746adc7a0ee78f648ce5
Matildus <53111606+Matildust95@users.noreply.github.com>
2020-03-02 08:39:56 +0000

    Add files via upload
    My website 'The Year You Were Born' allows you to search for any year between 1990 -2000, so you can read what happened the year you were born.

---
+++ main.js
@@ -0,0 +1,85 @@
+// index.html
+
+// Input field - enables user to put in any number and be redirected to that page
+
+function redirect() {
+    var answergiven = document.getElementById("answergiven");
+    var realanswer1 = document.getElementById("realanswer1");
+    var nextpage1 = document.getElementById("nextpage1");
+    var answergiven = document.getElementById("answergiven");
+    var realanswer2 = document.getElementById("realanswer2");
+    var nextpage2 = document.getElementById("nextpage2");
+    var answergiven = document.getElementById("answergiven");
+    var realanswer3 = document.getElementById("realanswer3");
+    var nextpage3 = document.getElementById("nextpage3");
+    var answergiven = document.getElementById("answergiven");
+    var realanswer4 = document.getElementById("realanswer4");
+    var nextpage4 = document.getElementById("nextpage4");
+    var answergiven = document.getElementById("answergiven");
+    var realanswer5 = document.getElementById("realanswer5");
+    var nextpage5 = document.getElementById("nextpage5");
+    var answergiven = document.getElementById("answergiven");
+    var realanswer6 = document.getElementById("realanswer6");
+    var nextpage6 = document.getElementById("nextpage6");
+    var answergiven = document.getElementById("answergiven");
+    var realanswer7 = document.getElementById("realanswer7");
+    var nextpage7 = document.getElementById("nextpage7");
+    var answergiven = document.getElementById("answergiven");
+    var realanswer8 = document.getElementById("realanswer8");
+    var nextpage8 = document.getElementById("nextpage8");
+    var answergiven = document.getElementById("answergiven");
+    var realanswer9 = document.getElementById("realanswer9");
+    var nextpage9 = document.getElementById("nextpage9");
+    var answergiven = document.getElementById("answergiven");
+    var realanswer10 = document.getElementById("realanswer10");
+    var nextpage10 = document.getElementById("nextpage10");
+    var answergiven = document.getElementById("answergiven");
+    var realanswer11 = document.getElementById("realanswer11");
+    var nextpage11 = document.getElementById("nextpage11");
+
+    if(answergiven.value.toLowerCase() == realanswer1.value.toLowerCase()){
+        window.location = nextpage1.value;
+    }
+
+    if(answergiven.value.toLowerCase() == realanswer2.value.toLowerCase()){
+        window.location = nextpage2.value;
+    }
+
+    if(answergiven.value.toLowerCase() == realanswer3.value.toLowerCase()){
+        window.location = nextpage3.value;
+    }
+
+    if(answergiven.value.toLowerCase() == realanswer4.value.toLowerCase()){
+        window.location = nextpage4.value;
+    }
+
+    if(answergiven.value.toLowerCase() == realanswer5.value.toLowerCase()){
+        window.location = nextpage5.value;
+    }
+
+    if(answergiven.value.toLowerCase() == realanswer6.value.toLowerCase()){
+        window.location = nextpage6.value;
+    }
+
+    if(answergiven.value.toLowerCase() == realanswer7.value.toLowerCase()){
+        window.location = nextpage7.value;
+    }
+
+    if(answergiven.value.toLowerCase() == realanswer8.value.toLowerCase()){
+        window.location = nextpage8.value;
+    }
+
+    if(answergiven.value.toLowerCase() == realanswer9.value.toLowerCase()){
+        window.location = nextpage9.value;
+    }
+
+    if(answergiven.value.toLowerCase() == realanswer10.value.toLowerCase()){
+        window.location = nextpage10.value;
+    }
+
+    if(answergiven.value.toLowerCase() == realanswer11.value.toLowerCase()){
+        window.location = nextpage11.value;
+    }
+
+    return false; // prevent further bubbling of event
+  }
